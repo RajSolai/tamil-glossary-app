d4e86a3cde0dacdb7fe65c5e5312a3f769848b5e
Solai Raj <msraj085@gmail.com>
2022-05-11 19:38:55 +0530

    added searchlabel

--- services/strings.ts
+++ services/strings.ts
@@ -1,13 +1,16 @@
-export const thankMessage:string = "வார்த்தை சேர்த்ததற்கு நன்றி"
-export const errorMessage:string = "அடடே, மீண்டும் முயற்சிக்கவும்"
-export const addNewWordTitle:string = "சொற்களஞ்சியத்தில் புதிய உள்ளீட்டைச் சேர்க்கவும்"
-export const addEnglishWordLabel:string = "ஆங்கில வார்த்தையைச் சேர்க்கவும்"
-export const addTamilWordLabel:string = "தொடர்புடைய தமிழ் வார்த்தையைச் சேர்க்கவும்"
-export const appTitle:string = "சொற்களஞ்சியம்"
-export const greetMessage:string = ""
-export const clickHereToAddWords:string = "வார்த்தைகளைச் சேர்க்கவும்"
-export const submitWord : string = "வார்த்தையை சமர்ப்பிக்கவும்"
-export const loginText : string = "உள்நுழைய"
-export const allRequestTitle: string = "அனைத்து வார்த்தைகள்"
-export const homePage : string = "முகப்பு"
-export const glossaryPage: string = "சொற்களஞ்சியம்"
+export const thankMessage: string = "வார்த்தை சேர்த்ததற்கு நன்றி";
+export const errorMessage: string = "அடடே, மீண்டும் முயற்சிக்கவும்";
+export const addNewWordTitle: string =
+  "சொற்களஞ்சியத்தில் புதிய உள்ளீட்டைச் சேர்க்கவும்";
+export const addEnglishWordLabel: string = "ஆங்கில வார்த்தையைச் சேர்க்கவும்";
+export const addTamilWordLabel: string =
+  "தொடர்புடைய தமிழ் வார்த்தையைச் சேர்க்கவும்";
+export const appTitle: string = "சொற்களஞ்சியம்";
+export const greetMessage: string = "";
+export const clickHereToAddWords: string = "வார்த்தைகளைச் சேர்க்கவும்";
+export const submitWord: string = "வார்த்தையை சமர்ப்பிக்கவும்";
+export const loginText: string = "உள்நுழைய";
+export const allRequestTitle: string = "அனைத்து வார்த்தைகள்";
+export const homePage: string = "முகப்பு";
+export const glossaryPage: string = "சொற்களஞ்சியம்";
+export const searchLabel: string = "அர்த்தங்களை இங்கே தேடுங்கள்";
